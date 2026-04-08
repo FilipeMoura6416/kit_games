fce75a2e269b2d22dc9f144881583ca8516977f5
Filipe Moura <lfsm.office@gmail.com>
2026-04-08 19:01:06 -0300

    Starting negamax

---
+++ advsearch/your_agent/negamax.py
@@ -0,0 +1,22 @@
+
+from typing import Tuple, Callable
+import datetime
+import time
+from ..tttm import board as board
+from datetime import datetime
+from ..othello.board import Board
+
+
+
+def minimax_move(state, time_amount, eval_func:Callable) -> Tuple[int, int]:
+    """
+    Returns a move computed by the minimax algorithm with alpha-beta pruning for the given game state.
+    :param state: state to make the move (instance of GameState)
+    :param max_depth: maximum depth of search (-1 = unlimited)
+    :param eval_func: the function to evaluate a terminal or leaf state (when search is interrupted at max_depth)
+                    This function should take a GameState object and a string identifying the player,
+                    and should return a float value representing the utility of the state for the player.
+    :return: (int, int) tuple with x, y coordinates of the move (remember: 0 is the first row/column)
+    """
+    return best_move
+
